113b1c326684a804b15c21b0616a3aa915cc9bab
mhthomas3 <morganhthomas93@gmail.com>
2021-04-26 08:20:01 -0500

    Added JS to update timestamp

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
+function formatDate(timestamp){
+    //calculate the date
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`
+
+}
+
 function displayTemperature(response){
-    console.log(response)
     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector(".current-weather-descrip").innerHTML = response.data.weather[0].description;
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);
+    document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000)
 
 }
 
