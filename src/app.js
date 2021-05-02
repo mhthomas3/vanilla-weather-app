ad89bf368533aaee9133856a4f0bb25ce933aa9d
mhthomas3 <morganhthomas93@gmail.com>
2021-05-02 18:35:14 -0500

    Added forecast API call and updated JS to inject correct forecast data into HTML

--- src/app.js
+++ src/app.js
@@ -16,26 +16,40 @@ function formatDate(timestamp){
 
 }
 
-function displayForecast(){
+function formatDay(timestamp){
+    let date = new Date(timestamp*1000);
+    let day = date.getDay()
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    return days[day];
+}
+
+function displayForecast(response){
+    let forecast = response.data.daily;
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class = "row">`;
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
-   
-    days.forEach(function(day){
+    
+    forecast.forEach(function(forecastDay, index){
+    if (index<6){
     
     forecastHTML = forecastHTML + `
         <div class="col-2"> 
-            <div class="forecast-day">${day}</div>
-            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="" width="36">
-            <span class="forecast-temp-max">70°</span> <span class="forecast-temp-min">50°</span>
+            <div class="forecast-day">${formatDay(forecastDay.dt)}</div>
+            <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="36">
+            <span class="forecast-temp-max">${Math.round(forecastDay.temp.max)}°</span> <span class="forecast-temp-min">${Math.round(forecastDay.temp.min)}°</span>
         </div>`
     ;
-    });
+    }});
 
 
     forecastHTML = forecastHTML + "</div>";
     forecastElement.innerHTML = forecastHTML
+}
+
+function getForecast(coordinates){
+    let apiKey = "0938aaea4eb798390f9b1df3fa43323f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response){
@@ -50,6 +64,8 @@ function displayTemperature(response){
     document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     document.querySelector("#icon").setAttribute("alt", `${response.data.weather[0].description}`);
     fahrenheitTemperature = response.data.main.temp;
+
+    getForecast(response.data.coord);
 }
 
 
@@ -125,5 +141,5 @@ let fahrenheitTemperature = null;
 
 //default function call
 search("New York");
-displayForecast();
+
 
